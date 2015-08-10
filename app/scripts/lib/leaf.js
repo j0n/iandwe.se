3eff6e18336df5b9037ae8d22d5775ebbb6a723a
Jon Andersson <andersson.jon@gmail.com>
2015-08-10 13:32:37 +0200

    latest version

--- app/scripts/lib/leaf.js
+++ app/scripts/lib/leaf.js
@@ -6,7 +6,7 @@ var straight = d3.svg.line()
 
 var svg = d3.select("svg")
 var g = svg.append('g');
-var push = 650;
+var push = 460;
 var rotate = push + 140;
 var groups =[];
 var amountOfLines = 6;
@@ -39,23 +39,23 @@ var leafData = [
 ]
 module.exports = {
     debug: function() {
-               console.log('debug');
         this.drawFlower(leafData, 0);
     },
     drawFlower: function(leafs, leafIndex) {
-                    console.log(leafs[leafIndex]);
         if (typeof leafs[leafIndex] !== 'undefined') {
-            this.drawLeaf(leafs[leafIndex], function() {
+            this.drawLeaf(leafs[leafIndex], function done() {
                 this.drawFlower(leafs, leafIndex+1);
             }.bind(this));
         }
     },
     drawLeaf: function(data, done) {
+                  var serverData = data.leafData; 
         var index = data.index;
-        for (var i = 0; i < amountOfLines; i++) {
+        for (var i = 0; i < data.leafData.length; i++) {
             var myData = JSON.parse(JSON.stringify(leafData));
             var size = 1 - data.value;
             var yCorrection = (myData[0].y - (myData[0].y * size));
+            //data[j].rand = data.leafData[j];
             for (var j = 0; j < myData.length; j++) {
                 myData[j].x += push;
                 if (j !== 0 && j !== 4){
@@ -73,19 +73,19 @@ module.exports = {
                     }
                 }
             }
-            (function(i, data) {
+            (function(i, data, rand) {
                 setTimeout(function() {
-                    this._makeLeaf(index, data);
-                    if (i === (amountOfLines -1)) {
+                    this._makeLeaf(index, data, i, rand);
+                    if (i === (serverData.length -3)) {
                         if (done) {
                             done();
                         }
                     }
-                }.bind(this), i * 700)
-            }.bind(this))(i, JSON.parse(JSON.stringify(myData)))
+                }.bind(this), i * 300)
+            }.bind(this))(i, JSON.parse(JSON.stringify(myData)), data.leafData[i])
         }
     },
-    _makeLeaf: function(index, leafD) {
+    _makeLeaf: function(index, leafD, leafIndex, rand) {
         var fix = 0
         var t =  0.6;
         var group;
@@ -100,13 +100,14 @@ module.exports = {
 
         var p = group.append("path")
             .data([t, t, t, t, t])
-            .attr("d", function(d) {
-                var a = (Math.round(Math.random() * 3)/30) + d;
-                return straight.tension(a)(leafD)})
+            .attr("d", function(d, a) {
+
+                var a = (Math.round(Math.random() * 50)/30) + d;
+                return straight.tension(d + rand)(leafD)})
             .style({
-                "stroke": 'black'
+                "stroke": 'rgba(0,0,0,'+(1-(leafIndex/8))+')'
             })
-            .attr('fill', 'rgba(0,0,0,0)')
+            .attr('fill', 'none')
             .attr('stroke-dasharray', function() { return (this.getTotalLength() + fix) +  " " + (this.getTotalLength() + fix); })
             .attr('stroke-dashoffset', function() { return this.getTotalLength() + fix; })
             .transition()
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,59 @@
 var d3 = require('d3');
 var svg = d3.select("svg")
 var io = require('socket.io-client');
-var socket = io('http://tuneset.com:3000');
+var socketUrl = 'http://tuneset.com:3000';
+var socket = io(socketUrl);
 var leaf = require('./lib/leaf.js');
+var input; 
+function addPental() {
+    var email = input.value;
+    var re = /\S+@\S+\.\S+/;
+    if (re.test(email)) {
+        var bodyRect = document.body.getBoundingClientRect(),
+            elemRect = svg[0][0].getBoundingClientRect(),
+            offset   = (elemRect.top - bodyRect.top) + (elemRect.height/2);
+        scrollTo(document.querySelector('body'), 100 , 300);
+        socket.emit('add', email);
+        input.value = "";
+    }
+    else {
+        alert('Not a valid email');
+    }
+}
+document.addEventListener('click', function() {
+});
+function scrollTo(element, to, duration) {
+    if (duration < 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
+window.onload = function() {
+    input = document.getElementById('email');
+    document.getElementById('create-leaf').addEventListener('click', addPental);
+    input.addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) {
+            addPental();
+        }
+    });
+}
+
+socket.on('connect', function() {
+    console.log('connected');
+});
 socket.on('all', function(data){
     leaf.drawFlower(data, 0);
 });
-socket.on('log', function(data) {
-    console.log('log', data);
+socket.on('full', function(data){
+    alert('The flower is now full, next flower will be released tomorrow');
 });
 socket.on('mail', function(data){
+    console.log('got mail', data);
     leaf.drawLeaf(data);
-    console.log('event', data);
 });
 socket.on('disconnect', function(){});
